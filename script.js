55da7d56416d35245b5c76f9b5095a9682f3f876
CHINTAMANI PALA <chintamanipala67@gmail.com>
2023-06-18 13:24:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+let inputfield=document.getElementById("inputfield")
+let buttons=document.getElementsByTagName("button")
+$("button").click(function() {
+    if(this.id=="back"){
+        let valueofin=inputfield.value;
+        let newval=valueofin.slice(0,length-1)
+        inputfield.value=newval
+    }else if(this.id=="ac"){
+        inputfield.value=""
+    }
+    else if(this.id=="equal"){
+        let result=eval(inputfield.value)
+        if(result===undefined){
+            inputfield.value="Error"
+        }else if(result=="function Error() { [native code] }"){
+            inputfield.value="Error"
+        }
+        else{
+
+            inputfield.value=result
+        }
+    }
+    else if(this.id=="percentile"){
+        let result= (+inputfield.value)/100
+        if(isNaN(result)){
+            inputfield.value="Error"
+        }else{
+        inputfield.value=result
+        }
+    }
+    else if(this.id=="mul"){
+        let b=document.getElementById(this.id)
+        inputfield.value=inputfield.value+""+b.value
+    }
+    else{
+        let b=document.getElementById(this.id)
+        inputfield.value=inputfield.value+""+b.innerText
+    }
+   
+});
