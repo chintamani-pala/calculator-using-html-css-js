82c80ddd6c6023f246cff0ff01b7a0b7ec60d7e9
CHINTAMANI PALA <chintamanipala67@gmail.com>
2023-06-18 15:14:09 +0530

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,43 +1,50 @@
-let inputfield=document.getElementById("inputfield")
-let buttons=document.getElementsByTagName("button")
-$("button").click(function() {
-    if(this.id=="back"){
-        let valueofin=inputfield.value;
-        let newval=valueofin.slice(0,length-1)
-        inputfield.value=newval
-    }else if(this.id=="ac"){
-        inputfield.value=""
-    }
-    else if(this.id=="equal"){
-        let result=eval(inputfield.value)
-        if(result===undefined){
-            inputfield.value="Error"
-        }else if(result=="function Error() { [native code] }"){
-            inputfield.value="Error"
-        }
-        else{
+let inputfield = document.getElementById("inputfield")
+let buttons = document.getElementsByTagName("button")
+$("button").click(function () {
 
-            inputfield.value=result
+    let valueofin = inputfield.value;
+    if (this.id == "back") {
+       
+        let newval = valueofin.slice(0, valueofin.length - 1)
+        inputfield.value = newval
+    } else if (this.id == "ac") {
+        inputfield.value = ""
+    }
+    else if (this.id == "equal") {
+        let result = eval(inputfield.value)
+        if (result === undefined) {
+            inputfield.value = "Error"
+        } else if (result == "function Error() { [native code] }") {
+            inputfield.value = "Error"
+        }
+        else {
+
+            inputfield.value = result
         }
     }
-    else if(this.id=="percentile"){
-        let result= (+inputfield.value)/100
-        if(isNaN(result)){
-            inputfield.value="Error"
-        }else{
-        inputfield.value=result
+    else if (this.id == "percentile") {
+        let result = (+inputfield.value) / 100
+        if (isNaN(result)) {
+            inputfield.value = "Error"
+        } else {
+            inputfield.value = result
         }
     }
-    else if(this.id=="mul"){
-        let b=document.getElementById(this.id)
-        inputfield.value=inputfield.value+""+b.value
-    }
-    else{
-        let b=document.getElementById(this.id)
-        if(inputfield.value=="Error"){
-         inputfield.value=""   
+    else if (this.id == "mul" || this.id == "div" || this.id == "sub" || this.id == "add") {
+        let b = document.getElementById(this.id)
+        if(inputfield.value[inputfield.value.length-1]=="+" || inputfield.value[inputfield.value.length-1]=="-" || inputfield.value[inputfield.value.length-1]=="*" || inputfield.value[inputfield.value.length-1]=="/"){
+            
+            inputfield.value=valueofin.slice(0, valueofin.length - 1)
+            inputfield.value = inputfield.value + "" + b.value
         }
-        inputfield.value=inputfield.value+""+b.innerText
+        if(!(inputfield.value[inputfield.value.length-1]==b.value)){
+            inputfield.value = inputfield.value + "" + b.value
+        }
+        
     }
-   
+    else {
+        let b = document.getElementById(this.id)
+        inputfield.value = inputfield.value + "" + b.innerText
+    }
+
 });
