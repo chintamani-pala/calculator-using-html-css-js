72e7af1fa3a422a589799354d0a9bb461f5a5a98
CHINTAMANI PALA <chintamanipala67@gmail.com>
2023-06-18 15:16:33 +0530

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -2,9 +2,8 @@ let inputfield = document.getElementById("inputfield")
 let buttons = document.getElementsByTagName("button")
 $("button").click(function () {
 
-    let valueofin = inputfield.value;
     if (this.id == "back") {
-       
+        let valueofin = inputfield.value;
         let newval = valueofin.slice(0, valueofin.length - 1)
         inputfield.value = newval
     } else if (this.id == "ac") {
@@ -33,8 +32,8 @@ $("button").click(function () {
     else if (this.id == "mul" || this.id == "div" || this.id == "sub" || this.id == "add") {
         let b = document.getElementById(this.id)
         if(inputfield.value[inputfield.value.length-1]=="+" || inputfield.value[inputfield.value.length-1]=="-" || inputfield.value[inputfield.value.length-1]=="*" || inputfield.value[inputfield.value.length-1]=="/"){
-            
-//             inputfield.value=valueofin.slice(0, valueofin.length - 1)
+            let valueofin = inputfield.value;
+            inputfield.value=valueofin.slice(0, valueofin.length - 1)
             inputfield.value = inputfield.value + "" + b.value
         }
         if(!(inputfield.value[inputfield.value.length-1]==b.value)){
